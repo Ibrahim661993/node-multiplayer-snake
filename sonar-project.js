979a10e763aac374e6c140d8366923057c0a0512
Ibrahim661993 <154541925+Ibrahim661993@users.noreply.github.com>
2025-07-08 11:46:54 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -12,9 +12,10 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner.scan(
   {
     serverUrl: 'http://192.168.56.4:9000',
-    token: process.env.SONAR_TOKEN,
+    //token: process.env.SONAR_TOKEN,
    
     options: {
+       'sonar.login': process.env.SONAR_TOKEN,
      // 'sonar.login':'8b523babbaac718a4ce12442a2c3bf3786088783',
       'sonar.sources': '.',
     }
